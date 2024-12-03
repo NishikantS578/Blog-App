f669da1fb2017afbdae5e696313f428ec2c4a8f4
Nishikant Singh <nishikantsingh2004@gmail.com>
2024-12-03 12:49:55 +0530

    Added underline to nav items.

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import Blog from './pages/Blog'
 function App() {
   return (
     <BrowserRouter>
-      <section className='flex flex-wrap my-4 px-4 items-center justify-center'>
+      <section className='flex flex-wrap my-4 px-4 items-center justify-center gap-4'>
         <div className='font-bold italic text-5xl silkscreen-bold'>
           <Link className='text-white hover:text-white flex' to="/">DevDiary</Link>
         </div>
         <nav className='min-[550px]:ml-auto flex items-center'>
-          <Link className='py-2 px-4 font-bold text-xl text-white hover:text-gray-200' to={"/"}>Home</Link>
-          <Link className='py-2 px-4 font-bold text-xl text-white hover:text-gray-200' to={"/write-blog"}>Write Blog</Link>
+          <Link className='py-2 px-4 font-bold text-xl text-white hover:text-gray-200 underline' to={"/"}>Home</Link>
+          <Link className='py-2 px-4 font-bold text-xl text-white hover:text-gray-200 underline' to={"/write-blog"}>Write Blog</Link>
         </nav>
       </section>
       <div className='px-4'>
